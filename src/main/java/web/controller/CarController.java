0e0cd188a23bcd834f57d44a2f542e4bd09effd9
Alex <locher91@mail.ru>
2024-03-28 20:28:01 +0300

    Last solution

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,22 +3,27 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
+import web.servise.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Controller
 public class CarController {
 
-    @GetMapping(value = "/cars")
-    public String getCar(ModelMap modelMap) {
-        List<String> messages = new ArrayList<>();
-        messages.add("Hello!");
-        messages.add("I'm Spring MVC application");
-        messages.add("5.2.0 version by sep'20 ");
-        modelMap.addAttribute("messages", messages);
+    @GetMapping("/cars")
+    public String getCars(@RequestParam(value = "count", defaultValue = "5") int allCars, ModelMap model) {
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("BMV", "Blue", 415));
+        list.add(new Car("Mersedes", "White", 180));
+        list.add(new Car("Lada", "Green", 2407));
+        list.add(new Car("Mazda", "Black", 5));
+        list.add(new Car("Mitsubisi", "Silver", 9));
+        list = CarServiceImpl.carsCount(list, allCars);
+        model.addAttribute("list", list);
         return "cars";
     }
-
-
 }
