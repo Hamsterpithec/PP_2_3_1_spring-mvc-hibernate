230fd79bb9beccfead7b05cfc092303839895e1e
Alex <locher91@mail.ru>
2024-03-29 17:50:59 +0300

    Last solution

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -4,26 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import web.dao.CarDao;
 import web.model.Car;
 import web.servise.CarServiceImpl;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Controller
 public class CarController {
+    private final CarServiceImpl carService;
+
+    private final CarDao carDao;
+
+    public CarController(CarServiceImpl carService, CarDao carDao) {
+        this.carService = carService;
+        this.carDao = carDao;
+    }
+
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int allCars, ModelMap model) {
-        List<Car> list = new ArrayList<>();
-        list.add(new Car("BMV", "Blue", 415));
-        list.add(new Car("Mersedes", "White", 180));
-        list.add(new Car("Lada", "Green", 2407));
-        list.add(new Car("Mazda", "Black", 5));
-        list.add(new Car("Mitsubisi", "Silver", 9));
-        list = CarServiceImpl.carsCount(list, allCars);
-        model.addAttribute("list", list);
+        List<Car> carList = carService.carsCount(allCars, carDao.initCar());
+        model.addAttribute("cars", carList);
         return "cars";
     }
 }
--- src/main/java/web/servise/CarServiceImpl.java
+++ src/main/java/web/servise/CarServiceImpl.java
@@ -1,14 +1,23 @@
 package web.servise;
+
+import org.springframework.stereotype.Component;
 import web.model.Car;
+
 import java.util.List;
 import java.util.stream.Collectors;
-
+@Component
 public class CarServiceImpl implements CarService {
 
 
-    public static List<Car> carsCount(List<Car> list, int number) {
-        if (number == 0 || number > 5) return list;
-        return list.stream().limit(number).collect(Collectors.toList());
+    public List<Car> carsCount(int number, List<Car> carList) {
+        List<Car> result;
+        if (number == 0 || number > 5) {
+            result = carList;
+        } else {
+            result = carList.stream().limit(number).collect(Collectors.toList());
+        }
+        return result;
     }
+
 
 }
