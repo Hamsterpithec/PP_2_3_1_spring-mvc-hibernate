badc39439c0058a0c1b6ea679a87c81dbb9c6ec9
Alex <locher91@mail.ru>
2024-04-02 21:18:46 +0300

    DB

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -7,20 +7,13 @@ import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao {
-    private final List<User> userList;
 
-    {
-        User user1 = new User(1, "Billy", "Smith");
-        User user2 = new User(2, "Jonny", "White");
-        User user3 = new User(3, "Garry", "Potter");
-        User user4 = new User(4, "Severus", "Snake");
-        User user5 = new User(5, "Sally", "Dolgopups");
-        userList = List.of(user1, user2, user3, user4, user5);
-    }
+
+
 
     @Override
     public List<User> initUser() {
-        return userList;
+        return null;
     }
 
 }
--- src/main/java/web/model/User.java
+++ src/main/java/web/model/User.java
@@ -2,24 +2,35 @@ package web.model;
 
 
 import org.springframework.stereotype.Component;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "users")
 @Component
 public class User {
-    
-    private int id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "last_name")
     private String lastname;
-    
+
+    @Column(name = "email")
+    private String email;
 
 
     public User() {
     }
 
-    public User(int id, String name, String lastname) {
-        this.id = id;
+    public User(String name, String lastname, String email) {
         this.name = name;
         this.lastname = lastname;
-        
+        this.email=email;
+
     }
 
     public String getName() {
@@ -38,11 +49,29 @@ public class User {
         this.lastname = lastname;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
