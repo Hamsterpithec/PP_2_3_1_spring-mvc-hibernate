8d73ed300f7748a2c5b352eefa7508c1d9105dd3
Alex <locher91@mail.ru>
2024-04-04 21:45:51 +0300

    04.04.24

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -24,52 +24,52 @@ public class UserController {
     }
 
 
-    @GetMapping
+    @GetMapping("/users")
     public String allUsers(Model model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("users", users);
-        return "user/users";
+        return "users";
     }
 
     @GetMapping("/new")
     public String createUserForm(@ModelAttribute("user") User user) {
         System.out.println("new user");
-        return "user/create_user";
+        return "create_user";
     }
 
     @PostMapping
     public String createUser(@ModelAttribute("user")  User user,
                              BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "user/create_user";
+            return "create_user";
         }
 
         userService.addUser(user);
-        return "redirect:/users";
+        return "users";
     }
 
     @GetMapping("/edit")
     public String editUserForm(@RequestParam("id") Long id, Model model) {
 
         model.addAttribute("user", userService.findById(id));
-        return "redirect:/users";
+        return "users";
     }
 
     @PostMapping("/edit")
     public String editUser(@ModelAttribute("user") User user,
                            BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "user/edit_user";
+            return "edit_user";
         }
 
         userService.updateUser(user);
-        return "redirect:/users";
+        return "users";
     }
 
     @PostMapping("/delete")
     public String deleteUser(@RequestParam("id") Long id) {
         userService.deleteUser(id);
-        return "redirect:/users";
+        return "users";
     }
 }
 
